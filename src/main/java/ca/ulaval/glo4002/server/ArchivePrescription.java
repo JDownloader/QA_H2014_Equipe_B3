2e77261ba47631062fa4dbef5fba0af532d9c031
orobert91 <o.robert@hotmail.com>
2014-01-25 11:01:30 -0500

    Code Review: Olivier R -> Vincent G
    Commentaires en début de fichier

--- src/main/java/ca/ulaval/glo4002/server/ArchivePrescription.java
+++ src/main/java/ca/ulaval/glo4002/server/ArchivePrescription.java
@@ -2,6 +2,14 @@ package ca.ulaval.glo4002.server;
 
 import java.util.ArrayList;
 
+/* CODE REVIEW 25/01/2014
+ * - Il faudrait définir une classe exception pour l'archivePrescription non trouvée (dans le style de ArchivePrescriptionNotFoundException)
+ * - Je suis pas trop sur de comprendre le sens de ton commentaire, le id est autoincrement quand le caller va faire un new Prescription avant de le passer à addPrescription, la classe ArchivePrescription n'a pas à se soucier du id..
+ * - Pour la technique itérative, d'accord si vous avez une meilleur façon de faire, en autant que ça soit aussi clean!
+ * 
+ * - Olivier R
+ */
+
 public class ArchivePrescription {
 
 	ArrayList<Prescription> archive;
