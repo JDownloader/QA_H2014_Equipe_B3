d5b6d95a357257c61b02f48da6ac6d0a211c8546
Tony G - PC <antoine.giasson.2@gmail.com>
2014-02-01 10:55:48 -0500

    surgeon

--- src/main/java/ca/ulaval/glo4002/staff/Surgeon.java
+++ src/main/java/ca/ulaval/glo4002/staff/Surgeon.java
@@ -1,5 +1,21 @@
 package ca.ulaval.glo4002.staff;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "SURGEON")
 public class Surgeon extends StaffMember {
 
+	@Id
+	@Column(name = "SURGEON_ID", nullable = false)
+	private int id;
+
+	public Surgeon(int id) {
+		super(id);
+	}
+
+	public int getId() {
+		return this.id;
+	}
 }
