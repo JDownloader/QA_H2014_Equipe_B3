a9382ceec7fc13cf10820cb8e9f8c339c4d29a1b
JDownloader <jeandanielpearson@gmail.com>
2014-03-25 20:59:08 -0400

    readd constant for config file

--- src/main/java/ca/ulaval/glo4002/rest/HospitalRestConfigProvider.java
+++ src/main/java/ca/ulaval/glo4002/rest/HospitalRestConfigProvider.java
@@ -8,13 +8,14 @@ import java.util.prefs.InvalidPreferencesFormatException;
 import java.util.prefs.Preferences;
 
 public class HospitalRestConfigProvider {
+	private static final String XML_CONFIG_FILE_NAME = "HospitalRestConfig.xml";
 	private static HospitalRestConfigProvider instance = null;
 	private Preferences prefs;
 
 	protected HospitalRestConfigProvider(Preferences prefs) {
 		this.prefs = prefs;
 		try {
-			InputStream is = new BufferedInputStream(new FileInputStream("HospitalRestConfigProvider.xml"));
+			InputStream is = new BufferedInputStream(new FileInputStream(XML_CONFIG_FILE_NAME));
 			Preferences.importPreferences(is);
 		} catch ( InvalidPreferencesFormatException e) {
 		} catch (IOException e) {
