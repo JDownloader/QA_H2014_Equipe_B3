8fea2e9a1c4d232d204c97f05773ce756c5ded3b
Marie-Hélène Bouchard <dkatanari@gmail.com>
2014-04-04 21:26:17 -0400

    Commencé à coder les Assemblers/Factories

--- src/main/java/ca/ulaval/glo4002/domain/intervention/InterventionFactory.java
+++ src/main/java/ca/ulaval/glo4002/domain/intervention/InterventionFactory.java
@@ -1,5 +1,12 @@
 package ca.ulaval.glo4002.domain.intervention;
 
+import ca.ulaval.glo4002.domain.patient.Patient;
+import ca.ulaval.glo4002.domain.staff.Surgeon;
+import ca.ulaval.glo4002.services.dto.InterventionCreationDTO;
+
 public class InterventionFactory {
-	//public Intervention createIntervention()
+	public Intervention createIntervention(InterventionCreationDTO dto, Surgeon surgeon, Patient patient) {
+		Intervention intervention = new Intervention();
+		return intervention;
+	}
 }
--- src/main/java/ca/ulaval/glo4002/services/assemblers/InterventionAssembler.java
+++ src/main/java/ca/ulaval/glo4002/services/assemblers/InterventionAssembler.java
@@ -1,5 +1,11 @@
 package ca.ulaval.glo4002.services.assemblers;
 
-public class InterventionAssembler {
+import ca.ulaval.glo4002.domain.intervention.Intervention;
+import ca.ulaval.glo4002.services.dto.InterventionCreationDTO;
 
+public class InterventionAssembler {
+	public Intervention assembleInterventionFromDTO(InterventionCreationDTO dto) {
+		Intervention intervention;
+		return intervention;
+	}
 }
