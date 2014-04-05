2da6f7fb228b2718bd669bf739555fb447c97088
Marie-Hélène Bouchard <dkatanari@gmail.com>
2014-04-04 21:43:12 -0400

    Deleted InterventionRessource along with its builder

--- src/main/java/ca/ulaval/glo4002/services/assemblers/InterventionAssembler.java
+++ src/main/java/ca/ulaval/glo4002/services/assemblers/InterventionAssembler.java
@@ -4,8 +4,8 @@ import ca.ulaval.glo4002.domain.intervention.Intervention;
 import ca.ulaval.glo4002.services.dto.InterventionCreationDTO;
 
 public class InterventionAssembler {
-	public Intervention assembleInterventionFromDTO(InterventionCreationDTO dto) {
-		Intervention intervention;
-		return intervention;
-	}
+//	public Intervention assembleInterventionFromDTO(InterventionCreationDTO dto) {
+//		Intervention intervention;
+//		return intervention;
+//	}
 }
