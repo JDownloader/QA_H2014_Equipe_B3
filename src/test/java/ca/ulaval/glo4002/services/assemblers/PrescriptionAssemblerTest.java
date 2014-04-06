6cf4b26c614eb8d9713ffa428e8f24bfc630ec57
Olivier Robert <o.robert@hotmail.com>
2014-04-06 13:22:58 -0400

    Remove empty assembler test

--- src/test/java/ca/ulaval/glo4002/services/assemblers/PrescriptionAssemblerTest.java
+++
@@ -1,9 +0,0 @@
-package ca.ulaval.glo4002.services.assemblers;
-
-import ca.ulaval.glo4002.domain.drug.DrugRepository;
-
-public class PrescriptionAssemblerTest {
-	private DrugRepository drugRepositoryMock;
-	
-	
-}
