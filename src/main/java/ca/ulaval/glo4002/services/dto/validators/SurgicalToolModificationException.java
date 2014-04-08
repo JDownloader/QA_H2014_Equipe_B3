6626dbbcdcf8b5337f9e89e3e3256e92c96d0b71
Vincent Lefebvre <vincent.l@live.ca>
2014-04-08 19:40:35 -0400

    Ajout classe d'exception

---
+++ src/main/java/ca/ulaval/glo4002/services/dto/validators/SurgicalToolModificationException.java
@@ -0,0 +1,15 @@
+package ca.ulaval.glo4002.services.dto.validators;
+
+public class SurgicalToolModificationException extends RuntimeException {
+
+	private static final long serialVersionUID = -8629051912534971560L;
+
+	public SurgicalToolModificationException() {
+		super();
+	}
+
+	public SurgicalToolModificationException(String message) {
+		super(message);
+	}
+	
+}
