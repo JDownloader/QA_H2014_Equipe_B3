56bcbb57af5165dfd4a6be1e4a3b6289d201a3ca
Marie-Hélène Bouchard <dkatanari@gmail.com>
2014-04-14 20:22:46 -0400

    Changed InvalidArgument exception

--- src/main/java/ca/ulaval/glo4002/exceptions/domainexceptions/InvalidArgument.java
+++ src/main/java/ca/ulaval/glo4002/exceptions/domainexceptions/InvalidArgument.java
@@ -4,8 +4,8 @@ package ca.ulaval.glo4002.exceptions.domainexceptions;
 public class InvalidArgument extends DomainException {
 	private static final long serialVersionUID = 1408594328944660822L;
 	
-	public InvalidArgument() {
-		this.code = "INT001";
-		this.message = "Erreur - informations manquantes ou invalides";
+	public InvalidArgument(String code, String message) {
+		this.code = code;
+		this.message = message;
 	}
 }
