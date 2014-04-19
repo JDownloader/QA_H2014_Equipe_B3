899d529f33ca04a067d32c34ca51935a88d1affa
Tony G - PC <antoine.giasson.2@gmail.com>
2014-04-19 04:50:20 -0400

    Correction de la derniere correction

--- src/main/java/ca/ulaval/glo4002/services/dto/validators/SurgicalToolModificationDTOValidator.java
+++ src/main/java/ca/ulaval/glo4002/services/dto/validators/SurgicalToolModificationDTOValidator.java
@@ -1,5 +1,7 @@
 package ca.ulaval.glo4002.services.dto.validators;
 
+import org.apache.commons.lang3.StringUtils;
+
 import ca.ulaval.glo4002.services.dto.SurgicalToolModificationDTO;
 
 public class SurgicalToolModificationDTOValidator {
@@ -7,7 +9,7 @@ public class SurgicalToolModificationDTOValidator {
 	public void validate(SurgicalToolModificationDTO surgicalToolModificationDTO) {
 		if (surgicalToolModificationDTO.newStatus == null) {
 			throw new DTOValidationException("Le paramètre 'statut' est requis.");
-		} else if (surgicalToolModificationDTO.newSerialNumber.equals("")) {
+		} else if (StringUtils.equals(surgicalToolModificationDTO.newSerialNumber, "")) {
 			throw new DTOValidationException("Le paramètre 'noserie' ne peut être vide.");
 		}
 	}
