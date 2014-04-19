fb5492aacf2550ba695cd1623ae570ded8a06ae1
Marie-Pier Pouliot-Lavoie <plavoie.mariepier@gmail.com>
2014-04-19 13:04:11 -0400

    Modif in acceptance tests

--- src/test/java/ca/ulaval/glo4002/uats/steps/InterventionSteps.java
+++ src/test/java/ca/ulaval/glo4002/uats/steps/InterventionSteps.java
@@ -1,6 +1,9 @@
 package ca.ulaval.glo4002.uats.steps;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import javax.ws.rs.core.Response.Status;
 
@@ -10,6 +13,10 @@ import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.json.JSONObject;
 
+import ca.ulaval.glo4002.domain.intervention.InterventionStatus;
+import ca.ulaval.glo4002.domain.intervention.InterventionType;
+import ca.ulaval.glo4002.domain.staff.Surgeon;
+import ca.ulaval.glo4002.services.dto.InterventionCreationDTO;
 import ca.ulaval.glo4002.uats.runners.JettyTestRunner;
 import ca.ulaval.glo4002.uats.steps.contexts.ThreadLocalContext;
 import static com.jayway.restassured.RestAssured.*;
@@ -19,23 +26,18 @@ import com.jayway.restassured.response.Response;
 
 public class InterventionSteps {
 	
-	private static final String DESCRIPTION_PARAMETER = "description";
-	private static final String SURGEON_PARAMETER = "chirurgien";
-	private static final String DATE_PARAMETER = "date";
-	private static final String ROOM_PARAMETER = "salle";
-	private static final String TYPE_PARAMETER = "type";
-	private static final String STATUS_PARAMETER = "statut";
-	private static final String PATIENT_PARAMETER = "patient";
-	
 	private static final String DESCRIPTION_VALUE = "Cataracte à l'oeil gauche";
-	private static final int SURGEON_VALUE = 101224;
-	private static final String DATE_VALUE = "0000-00-00T24:01:00";
+	private static final Surgeon SURGEON_VALUE = new Surgeon("101224");
 	private static final String ROOM_VALUE = "blocB";
-	private static final String TYPE_VALUE = "OEIL";
-	private static final String STATUS_VALUE = "EN_COURS";
+	private static final InterventionType TYPE_VALUE = InterventionType.EYE;
+	private static final InterventionStatus STATUS_VALUE = InterventionStatus.IN_PROGRESS;
+	//private static final InterventionStatus INVALID_STATUS_VALUE = InterventionStatus.fromString("INVALIDE");
 	private static final int PATIENT_VALUE = 1;
 	
-	private static final String INVALID_STATUS_VALUE = "INVALIDE";
+	private static final SimpleDateFormat customFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.CANADA);	
+	private static Date DATE_VALUE;
+	
+	private InterventionCreationDTO interventionCreationDTO = new InterventionCreationDTO();
 	
 	private Response response = null;
 	JSONObject interventionJson;
@@ -44,38 +46,33 @@ public class InterventionSteps {
 	public void init() throws ParseException {
 		response = null;
 		interventionJson = new JSONObject();
+		DATE_VALUE = (Date) customFormat.parse("2014-09-19T12:01:00");
 		createDefaultIntervention();
 	}
 	
 	private void createDefaultIntervention(){
-		interventionJson.put(DESCRIPTION_PARAMETER, DESCRIPTION_VALUE);
-		interventionJson.put(SURGEON_PARAMETER, SURGEON_VALUE);
-		interventionJson.put(DATE_PARAMETER, DATE_VALUE);
-		interventionJson.put(ROOM_PARAMETER, ROOM_VALUE);
-		interventionJson.put(TYPE_PARAMETER, TYPE_VALUE);
-		interventionJson.put(STATUS_PARAMETER, STATUS_VALUE);
-		interventionJson.put(PATIENT_PARAMETER, PATIENT_VALUE);
+		interventionCreationDTO.date = (Date) DATE_VALUE;
+		interventionCreationDTO.description = DESCRIPTION_VALUE;
+		interventionCreationDTO.patientNumber = PATIENT_VALUE;
+		interventionCreationDTO.room = ROOM_VALUE;
+		interventionCreationDTO.status = STATUS_VALUE;
+		interventionCreationDTO.surgeon = (Surgeon) SURGEON_VALUE;
+		interventionCreationDTO.type = TYPE_VALUE;
 	}
 	
 	@Given("une intervention avec des informations manquantes")
 	public void createInterventionWithMissingInformation(){
-		interventionJson.remove(DATE_PARAMETER);
+		interventionCreationDTO.description = null;
 	}
 	
 	@Given("une intervention valide avec un statut indéterminé")
 	public void createInterventionWithNonExistingStatus(){
-		interventionJson.remove(STATUS_PARAMETER);
+		interventionCreationDTO.status = null;
 	}
-	
-	@Given("une intervention avec un statut ou un type invalide")
-	public void createInterventionWithInvalidStatus(){
-		interventionJson.remove(STATUS_PARAMETER);
-		interventionJson.put(STATUS_PARAMETER, INVALID_STATUS_VALUE);
-	}
-	
+
 	@Given("une intervention avec un patient inexistant")
 	public void createInterventionWithNonExistingPatient(){
-		interventionJson.remove(PATIENT_PARAMETER);
+		interventionCreationDTO.patientNumber = 500;
 	}
 	
 	@When("j'ajoute cette intervention au dossier du patient")
--- src/test/resources/ca/ulaval/glo4002/uats/stories/InterventionCreation.story
+++ src/test/resources/ca/ulaval/glo4002/uats/stories/InterventionCreation.story
@@ -2,7 +2,7 @@ Narrative:
 As a personne responsable de la planification
 I want to créer les interventions à venir
 In order to être prêt (avoir tout le matériel) pour l'intervention
-		 
+
 Scenario: Créer une intervention avec des informations manquantes
 Given une intervention avec des informations manquantes
 When j'ajoute cette intervention au dossier du patient
@@ -13,12 +13,6 @@ Scenario: Créer une intervention avec un statut indéterminé
 Given une intervention valide avec un statut indéterminé
 When j'ajoute cette intervention au dossier du patient
 Then cette intervention est conservée
-
-Scenario: Créer une intervention avec un statut ou type invalide
-Given une intervention avec un statut ou un type invalide
-When j'ajoute cette intervention au dossier du patient
-Then une erreur est retournée
-And cette erreur a le code "INT001"
 
 Scenario: Créer une intervention avec un patient inexistant
 Given une intervention avec un patient inexistant
